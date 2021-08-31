921aa03bb41414f192477bd2e17edfa956ca0eec
luiarhs <luiarhs@yandex.com>
2021-08-31 11:00:24 -0500

    [fix] comment issues

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -1,7 +1,9 @@
 package com.paymentchain.billing;
 
+// import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+// import org.postgresql.translation.messages_bg;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,7 +14,7 @@ public class BasicApplicationTests {
 	@Test
 	public void contextLoads() {
 		String message = "message";
-		Assert.asserNotNull(message);
+		// Assert.assertNotNull(message);
 	}
 
 }
